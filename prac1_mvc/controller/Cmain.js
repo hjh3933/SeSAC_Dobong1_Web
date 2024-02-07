690c1ca29abf3a4ee3f5fa4dd820a3a30c7552e2
hjh3933 <hkh3933@naver.com>
2024-02-07 16:23:25 +0900

    0207AddCommit

--- prac1_mvc/controller/Cmain.js
+++ prac1_mvc/controller/Cmain.js
@@ -17,6 +17,35 @@ exports.login = (req, res) => {
   }
   res.send({
     result: result,
-    userInfo: userInfo,
+    userInfo: req.body,
+  });
+};
+
+//model에서 user값을 받아오기
+const Users = model.user;
+const userSplit = Users.split("\n");
+//엔터를 기준으로 3가지 (아이디 비밀번호 이름)을 가진 배열로 반환되었을 것
+//for문으로 3번을 비교하는 방식?
+userSplit[0];
+userSplit[1];
+userSplit[2];
+exports.login2 = (req, res) => {
+  console.log(userSplit);
+  console.log(req.body);
+  let name;
+  for (i = 0; i < userSplit.length; i++) {
+    let user = userSplit[i].split("//");
+    // [0] id, [1] pw, [2] name
+    if (user[0] == req.body.id && user[1] == req.body.pw) {
+      result = true;
+      name = user[2];
+      break;
+    } else {
+      result = false;
+    }
+  }
+  res.send({
+    result: result,
+    name: name,
   });
 };
--- prac1_mvc/routes/index.js
+++ prac1_mvc/routes/index.js
@@ -4,5 +4,6 @@ const controller = require("../controller/Cmain");
 
 router.get("/", controller.main);
 router.post("/login", controller.login);
+router.post("/login2", controller.login2);
 
 module.exports = router;

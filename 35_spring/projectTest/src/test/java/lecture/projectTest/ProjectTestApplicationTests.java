b52b16eccda850772b8da1f140848dc7a87422ba
hjh3933 <hkh3933@naver.com>
2024-05-31 17:39:02 +0900

    image test

---
+++ 35_spring/projectTest/src/test/java/lecture/projectTest/ProjectTestApplicationTests.java
@@ -0,0 +1,13 @@
+package lecture.projectTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
